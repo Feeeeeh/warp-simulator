2533bb66aa9b1ebebd8fc833aac965aab76766c1
Feeeeeh <klitzketech@gmail.com>
2024-07-18 15:04:05 -0300

    abas e imagens adicionados

--- test.py
+++ test.py
@@ -18,22 +18,17 @@ notebook.pack(expand=True, fill='both')
 # Primeira aba
 firefly_tab = Frame(notebook,width=200,height=200)
 notebook.add(firefly_tab, text="Firefly", padding=10) 
-firefly_banner = ImageTk.PhotoImage(Image.open("imagens/firefly_banner.png"))
-tk.Label(firefly_tab, image=firefly_banner).pack()
+#image1 = Image.open("<imagens/firefly_banner.png>")
+tk.Label(firefly_tab, text="awoooooooooo").pack()
 
 
 # Segunda aba
 cone_tab = Frame(notebook,width=200,height=200)
-notebook.add(cone_tab, text="Light Cone", padding=10)
-cone_banner = ImageTk.PhotoImage(Image.open("imagens/cone_banner.png"))
-tk.Label(cone_tab, image=cone_banner).pack()  
+notebook.add(cone_tab, text="Light Cone", padding=10)  
 
 # Terceira aba
 standard_tab = Frame(notebook,width=200,height=200)
-notebook.add(standard_tab, text="Standard", padding=10)
-standard_banner = ImageTk.PhotoImage(Image.open("imagens/standard_banner.png"))
-tk.Label(standard_tab, image=standard_banner).pack()  
-    
+notebook.add(standard_tab, text="Standard", padding=10)  
 
 
 root.mainloop()
