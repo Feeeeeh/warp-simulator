7f9f97a752fdda63694760d9c3c4e04bf8b434e0
Feeeeeh <klitzketech@gmail.com>
2024-07-18 17:14:44 -0300

    botões adicionados

--- main.py
+++ main.py
@@ -21,27 +21,54 @@ style.configure('TNotebook.Tab', width=15)
 # Notebook (abas)
 notebook = Notebook(style='custom.TNotebook')
 notebook.pack(expand=True, fill='both') 
+image_references = []
+def firefly(): # Primeira aba
+    firefly_tab = Frame(notebook,width=200,height=200)
+    notebook.add(firefly_tab, text="Firefly", padding=10) 
+    firefly_banner = ImageTk.PhotoImage(Image.open("imagens/firefly_banner.png"))
+    image_references.append(firefly_banner)
+    Label(firefly_tab, image=firefly_banner).pack()
+    frame = Frame(firefly_tab,bootstyle="dark")
+    frame.pack(side="bottom",fill="both")
+    button1 = Button(firefly_tab,bootstyle="dark",text="Warp 1x")
+    button10 = Button(firefly_tab,bootstyle="dark",text="Warp 10x")
+    button1.pack(in_=frame,anchor='s',side='left',fill="both",expand=True)
+    button10.pack(in_=frame,anchor='s',side='right',fill="both",expand=True)
 
-# Primeira aba
-firefly_tab = Frame(notebook,width=200,height=200)
-notebook.add(firefly_tab, text="Firefly", padding=10) 
-firefly_banner = ImageTk.PhotoImage(Image.open("imagens/firefly_banner.png"))
-tk.Label(firefly_tab, image=firefly_banner).pack()
+def cone(): # Segunda aba
+    cone_tab = Frame(notebook,width=200,height=200)
+    notebook.add(cone_tab, text="Light Cone", padding=10)
+    cone_banner = ImageTk.PhotoImage(Image.open("imagens/cone_banner.png"))
+    image_references.append(cone_banner)
+    Label(cone_tab, image=cone_banner).pack()  
+    frame = Frame(cone_tab,bootstyle="dark")
+    frame.pack(side="bottom",fill="both")
+    button1 = Button(cone_tab,bootstyle="dark",text="Warp 1x")
+    button10 = Button(cone_tab,bootstyle="dark",text="Warp 10x")
+    button1.pack(in_=frame,anchor='s',side='left',fill="both",expand=True)
+    button10.pack(in_=frame,anchor='s',side='right',fill="both",expand=True)
 
-
-# Segunda aba
-cone_tab = Frame(notebook,width=200,height=200)
-notebook.add(cone_tab, text="Light Cone", padding=10)
-cone_banner = ImageTk.PhotoImage(Image.open("imagens/cone_banner.png"))
-tk.Label(cone_tab, image=cone_banner).pack()  
-
-# Terceira aba
-standard_tab = Frame(notebook,width=200,height=200)
-notebook.add(standard_tab, text="Standard", padding=10)
-warp = Image.open("imagens/Stellar_Warp.png")
-warp1 = warp.resize((700,387))
-warp_r = ImageTk.PhotoImage(warp1)
-tk.Label(standard_tab, image=warp_r).pack()
+def standard(): # Terceira aba
+    standard_tab = Frame(notebook,width=200,height=200)
+    notebook.add(standard_tab, text="Standard", padding=10)
+    warp = Image.open("imagens/Stellar_Warp.png")
+    warp1 = warp.resize((700,387))
+    warp_r = ImageTk.PhotoImage(warp1)
+    image_references.append(warp_r)
+    Label(standard_tab, image=warp_r).pack()
+    frame = Frame(standard_tab,bootstyle="dark")
+    frame.pack(side="bottom",fill="both")
+    button1 = Button(standard_tab,bootstyle="dark",text="Warp 1x")
+    button10 = Button(standard_tab,bootstyle="dark",text="Warp 10x")
+    button1.pack(in_=frame,anchor='s',side='left',fill="both",expand=True)
+    button10.pack(in_=frame,anchor='s',side='right',fill="both",expand=True)
     
+    
+def main():
+    firefly()
+    cone()
+    standard()
+    root.mainloop()
 
-root.mainloop()
+if __name__ == "__main__":
+    main()
--- test.py
+++ test.py
@@ -2,6 +2,13 @@ import tkinter as tk
 from ttkbootstrap import *
 from PIL import ImageTk, Image 
 
+def pull1():
+    pass
+
+def pull10():
+    pass
+
+
 root = tk.Tk()
 root.title("Warp Simulator")
 root.geometry("800x500")
@@ -14,23 +21,46 @@ style.configure('TNotebook.Tab', width=15)
 # Notebook (abas)
 notebook = Notebook(style='custom.TNotebook')
 notebook.pack(expand=True, fill='both') 
+image_references = []
+def firefly(): # Primeira aba
+    firefly_tab = Frame(notebook,width=200,height=200)
+    notebook.add(firefly_tab, text="Firefly", padding=10) 
+    firefly_banner = ImageTk.PhotoImage(Image.open("imagens/firefly_banner.png"))
+    image_references.append(firefly_banner)
+    Label(firefly_tab, image=firefly_banner).pack()
+    frame = Frame(firefly_tab,bootstyle="dark")
+    frame.pack(side="bottom",fill="both")
+    button1 = Button(firefly_tab,bootstyle="dark",text="Warp 1x")
+    button10 = Button(firefly_tab,bootstyle="dark",text="Warp 10x")
+    button1.pack(in_=frame,anchor='s',side='left',fill="both",expand=True)
+    button10.pack(in_=frame,anchor='s',side='right',fill="both",expand=True)
 
-# Primeira aba
-firefly_tab = Frame(notebook,width=200,height=200)
-notebook.add(firefly_tab, text="teste", padding=10) 
-image1 = Image.open("imagens/Stellar_Warp.png")
-resized = image1.resize((700,500))
-resized_final = ImageTk.PhotoImage(resized)
-tk.Label(firefly_tab, image=resized_final).pack()
+    button1 = Button(firefly_tab,bootstyle="dark",text="Warp 1x")
+    button10 = Button(firefly_tab,bootstyle="dark",text="Warp 10x")
+    button1.pack(anchor='s',side='left')
+    button10.pack(anchor='s',side='right')
 
+def cone(): # Segunda aba
+    cone_tab = Frame(notebook,width=200,height=200)
+    notebook.add(cone_tab, text="Light Cone", padding=10)
+    cone_banner = ImageTk.PhotoImage(Image.open("imagens/cone_banner.png"))
+    image_references.append(cone_banner)
+    Label(cone_tab, image=cone_banner).pack()  
 
-# Segunda aba
-cone_tab = Frame(notebook,width=200,height=200)
-notebook.add(cone_tab, text="Light Cone", padding=10)  
+def standard(): # Terceira aba
+    standard_tab = Frame(notebook,width=200,height=200)
+    notebook.add(standard_tab, text="Standard", padding=10)
+    warp = Image.open("imagens/Stellar_Warp.png")
+    warp1 = warp.resize((700,387))
+    warp_r = ImageTk.PhotoImage(warp1)
+    image_references.append(warp_r)
+    Label(standard_tab, image=warp_r).pack()
+    
+def main():
+    firefly()
+    cone()
+    standard()
+    root.mainloop()
 
-# Terceira aba
-standard_tab = Frame(notebook,width=200,height=200)
-notebook.add(standard_tab, text="Standard", padding=10)  
-
-
-root.mainloop()
+if __name__ == "__main__":
+    main()
