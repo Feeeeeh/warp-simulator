fcce49246d60d0e04f4fa16aa47cf9e84f71d37e
Feeeeeh <klitzketech@gmail.com>
2024-07-24 17:24:45 -0300

    deus, dai-me força

--- main.py
+++ main.py
@@ -6,42 +6,79 @@ import mysql.connector
 import random
 
 class WarpSimulator:
-    def __init__(self, root): 
-        # cria a janela, seta um titulo e o tamanho dela
+    def __init__(self, root):
         self.root = root
-        self.root.title("Warp Simulator") 
-        self.root.geometry("800x500") 
-        root.resizable(False, False) # evita que o usuario mude o tamanho da janela 
-
-
-        # Estiliação da janela no estilo darkly na biblioteca ttkbootstrap
+        self.root.title("Warp Simulator")
+        self.root.geometry("800x500")
+        root.resizable(False, False)
+        
         self.style = Style(theme='darkly')
         self.style.configure('custom.TNotebook', tabposition="wn", padding=[5, 5])
         self.style.configure('TNotebook.Tab', width=15)
 
-        # Notebook para trocar entre abas
         self.notebook = Notebook(style='custom.TNotebook')
         self.notebook.pack(expand=True, fill='both')
         
-        # lista com a referencia das imagens, é necessario pra imagem se manter no loop principal, se não ela é criada e jogada fora
         self.image_references = []
 
-        # Itens do gacha
-        self.firefly_pulls = ["imagens/firefly_pull.png"]
-        self.cone_pulls = ["imagens/firefly_cone.png"]
-        self.standard_pulls = ["imagens/welt_pull.png"]
+        self.db_conn = self.connect_to_database()
+        self.firefly_pulls = self.get_data_from_db("gacha_ff")
+        self.cone_pulls = self.get_data_from_db("gacha_arma")
+        self.standard_pulls = self.get_data_from_db("gacha_base")
 
-        self.fila = [] #lista do que você tirou no gacha
-        self.current_10x_index = 0  # index que irá aumentar de 1 em 1 para pegar os itens da lista um por vez
+        self.image_mappings = self.create_image_mappings()
+
+        self.fila = []
+        self.current_10x_index = 0
 
         self.create_tabs()
 
-    def create_tabs(self): #cria as abas do notebook
-        self.create_tab("Firefly", "imagens/firefly_banner.png", lambda: self.pull1x(self.firefly_pulls), lambda: self.pull10x(self.firefly_pulls)) # firefly banner
-        self.create_tab("Light Cone", "imagens/cone_banner.png", lambda: self.pull1x(self.cone_pulls), lambda: self.pull10x(self.cone_pulls)) # light cone banner
-        self.create_standard_tab("Standard", "imagens/Stellar_Warp.png", lambda: self.pull1x(self.standard_pulls), lambda: self.pull10x(self.standard_pulls)) # standard banner
+    def connect_to_database(self):
+        try:
+            conn = mysql.connector.connect(
+                host="localhost",
+                user="root",
+                password="",
+                database="HSR"
+            )
+            return conn
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return None
 
-    def create_tab(self, title, banner_image_path, pull1x_command, pull10x_command): # tab da firefly e light cone por enquanto
+    def get_data_from_db(self, table_name):
+        try:
+            cursor = self.db_conn.cursor()
+            cursor.execute(f"SELECT nome FROM {table_name}")
+            results = cursor.fetchall()
+            return [row[0] for row in results]
+        except mysql.connector.Error as err:
+            print(f"Error: {err}")
+            return []
+
+    def create_image_mappings(self):
+        return {
+            'qiqi' : 'imagens/qiqi_pull.png',
+            'firefly1': 'imagens/firefly_pull.png',
+            'firefly2': 'imagens/firefly_pull.png',
+            'firefly3': 'imagens/firefly_pull.png',
+            'firefly4': 'imagens/firefly_pull.png',
+            'arma1': 'imagens/firefly_cone.png',
+            'arma2': 'imagens/firefly_cone.png',
+            'arma3': 'imagens/firefly_cone.png',
+            'arma4': 'imagens/firefly_cone.png',
+            'base1': 'imagens/welt_pull.png',
+            'base2': 'imagens/welt_pull.png',
+            'base3': 'imagens/welt_pull.png',
+            'base4': 'imagens/welt_pull.png'
+        }
+
+    def create_tabs(self):
+        self.create_tab("Firefly", "imagens/firefly_banner.png", lambda: self.pull1x(self.firefly_pulls), lambda: self.pull10x(self.firefly_pulls))
+        self.create_tab("Light Cone", "imagens/cone_banner.png", lambda: self.pull1x(self.cone_pulls), lambda: self.pull10x(self.cone_pulls))
+        self.create_standard_tab("Standard", "imagens/Stellar_Warp.png", lambda: self.pull1x(self.standard_pulls), lambda: self.pull10x(self.standard_pulls))
+
+    def create_tab(self, title, banner_image_path, pull1x_command, pull10x_command):
         tab = Frame(self.notebook, width=200, height=200)
         img = ImageTk.PhotoImage(Image.open("imagens/firefly_icon.jpg"))
         self.image_references.append(img)
@@ -51,17 +88,17 @@ class WarpSimulator:
         tk.Label(tab, image=banner_image).pack()
         self.create_buttons(tab, pull1x_command, pull10x_command)
 
-    def create_standard_tab(self, title, banner_image_path, pull1x_command, pull10x_command): # tab standard
+    def create_standard_tab(self, title, banner_image_path, pull1x_command, pull10x_command):
         tab = Frame(self.notebook, width=200, height=200)
         self.notebook.add(tab, text=title, padding=10)
         warp = Image.open(banner_image_path)
-        warp_resized = warp.resize((700, 387)) # resize da imagem do standard, ela era muito grande
+        warp_resized = warp.resize((700, 387))
         warp_image = ImageTk.PhotoImage(warp_resized)
         self.image_references.append(warp_image)
         tk.Label(tab, image=warp_image).pack()
         self.create_buttons(tab, pull1x_command, pull10x_command)
 
-    def create_buttons(self, tab, pull1x_command, pull10x_command): #cria a base dos botões, recebe argumentos pra mudar o comando dos botões 
+    def create_buttons(self, tab, pull1x_command, pull10x_command):
         frame = Frame(tab, bootstyle="dark")
         frame.pack(side="bottom", fill="both")
         button1 = Button(tab, bootstyle="dark", text="Warp 1x", command=pull1x_command)
@@ -69,74 +106,71 @@ class WarpSimulator:
         button1.pack(in_=frame, anchor='s', side='left', fill="both", expand=True)
         button10.pack(in_=frame, anchor='s', side='right', fill="both", expand=True)
 
-    def pull1x(self,x): # 1 tentativa de gacha
+    def pull1x(self, x):
         print("1 pull")
         resultado = random.choice(x)
         print(resultado)
         self.show_gif_and_result(resultado)
 
-    def pull10x(self,x): # 10 tentativas de gacha
+    def pull10x(self, x):
         print("10 pulls")
         self.fila = [random.choice(x) for _ in range(10)]
         print(self.fila)
-        self.current_10x_index = 0  # reseta o index caso não seja a primeira vez que você está usando o comando
+        self.current_10x_index = 0
         self.show_gif_and_result(self.fila)
 
-    def show_gif_and_result(self, resultado): # mostra o gif e em seguida chama a função de mostrar o resultado
+    def show_gif_and_result(self, resultado):
         new_window = tk.Toplevel(self.root)
-        new_window.resizable(False, False) # evita que o usuario mude o tamanho da janela
-        gif_path = "imagens/qiqi.gif" if "imagens/qiqi_pull.png" in resultado else "imagens/5_estrelas.gif" if "imagens/firefly_pull.png" in resultado else "imagens/3_estrelas.gif"
+        new_window.resizable(False, False)
+        gif_path = "imagens/qiqi.gif" if "qiqi" in resultado else "imagens/5_estrelas.gif" if "firefly" in resultado else "imagens/3_estrelas.gif"
   
-        # chama uma classe que comprime e junta os frames do gif de forma com que ele apareça direito, sem pontos vazios
-        gif = AnimatedGif(new_window, gif_path, loop=False, on_complete=lambda: self.show_result(new_window, resultado)) # não funciona sem lambda por algum motivo
+        gif = AnimatedGif(new_window, gif_path, loop=False, on_complete=lambda: self.show_result(new_window, resultado))
         gif.pack()
 
-    def show_result(self, window, result): # define qual imagem será mostrada na tela de resultado e envia para a função que cria a tela de resultado
-        if isinstance(result, str):  # 1 pull, verifica se a variavel resultado é uma string
-            result_image_path = result
+    def show_result(self, window, result):
+        if isinstance(result, str):
+            result_image_path = self.image_mappings.get(result, "imagens/lixo_lc.png")
             self.create_result_window(window, result_image_path)
-        else:  # 10 pull, caso ela for uma lista e não uma string
-            if self.current_10x_index < len(result): # enquanto aquele index lá de trás for menor que o tamanho da lista
-                result_image_path = result[self.current_10x_index]
+        else:
+            if self.current_10x_index < len(result):
+                result_image_path = self.image_mappings.get(result[self.current_10x_index], "imagens/lixo_lc.png")
                 self.create_result_window(window, result_image_path)
-                self.current_10x_index += 1 # aumenta o index lá de trás
+                self.current_10x_index += 1
             else:
-                self.close_all_windows() # caso o index seja igual ou maior que o tamanho da lista, fecha as abas
+                self.close_all_windows()
 
-    def create_result_window(self, window, image_path): # cria a tela de resultado com a informação da função anterior
-        result_window = tk.Toplevel(window) 
-        result_image = ImageTk.PhotoImage(Image.open(image_path)) # carrega a imagem do resultado
+    def create_result_window(self, window, image_path):
+        result_window = tk.Toplevel(window)
+        result_image = ImageTk.PhotoImage(Image.open(image_path))
         
-        # tentando limitar a imagem a um frame pra ela parar de mudar de tamanho, não funcionou 
         img_frame = Frame(result_window, width=600, height=300)
-        img_frame.pack(fill="both",expand=True)
-        img_frame.propagate(False) # evita que a frame aumente de tamanho baseado nas imagens
+        img_frame.pack(fill="both", expand=True)
+        img_frame.propagate(False)
         
-    
-        label = tk.Label(img_frame, image=result_image) # joga a imagem na tela
+        label = tk.Label(img_frame, image=result_image)
         label.pack()
-        self.image_references.append(result_image) # jogando a imagem pras referencias pra ela n sumir
+        self.image_references.append(result_image)
         
         frame = Frame(result_window, bootstyle="dark")
         frame.pack(side="bottom", fill="both")
         
-        next_button = Button(result_window, bootstyle="light", text="Next", command=lambda: self.proximo(result_window, label)) # botão que chama a função proximo
+        next_button = Button(result_window, bootstyle="light", text="Next", command=lambda: self.proximo(result_window, label))
         next_button.pack(in_=frame, anchor='s', side='left', fill="both", expand=True)
 
-    def proximo(self, result_window, label): # troca para a proxima imagem
-        if self.current_10x_index < len(self.fila): # checa se o index lá de trás é menor que o tamanho da lista
-            result_image_path = self.fila[self.current_10x_index] # vai se referir ao item da lista baseado no index
+    def proximo(self, result_window, label):
+        if self.current_10x_index < len(self.fila):
+            result_image_path = self.image_mappings.get(self.fila[self.current_10x_index], "imagens/lixo_lc.png")
             new_image = ImageTk.PhotoImage(Image.open(result_image_path))
             self.image_references.append(new_image)
-            label.config(image=new_image) # reconfigura a imagem da label para a nova
+            label.config(image=new_image)
             self.current_10x_index += 1
         else:
             self.close_all_windows()
 
     def close_all_windows(self):
-        for window in self.root.winfo_children(): # winfo_children() se refere a todas as janelas criadas a partir de outra
-            if isinstance(window, tk.Toplevel): # verifica se a variavel window é uma nova janela tk
-                window.destroy() # mata as criança :) 
+        for window in self.root.winfo_children():
+            if isinstance(window, tk.Toplevel):
+                window.destroy()
 
 def main():
     root = tk.Tk()
---
+++ old_main.py
@@ -0,0 +1,147 @@
+import tkinter as tk
+from ttkbootstrap import Style, Frame, Button, Notebook
+from PIL import ImageTk, Image
+from gif_loader import AnimatedGif
+import mysql.connector
+import random
+
+class WarpSimulator:
+    def __init__(self, root): 
+        # cria a janela, seta um titulo e o tamanho dela
+        self.root = root
+        self.root.title("Warp Simulator") 
+        self.root.geometry("800x500") 
+        root.resizable(False, False) # evita que o usuario mude o tamanho da janela 
+
+
+        # Estiliação da janela no estilo darkly na biblioteca ttkbootstrap
+        self.style = Style(theme='darkly')
+        self.style.configure('custom.TNotebook', tabposition="wn", padding=[5, 5])
+        self.style.configure('TNotebook.Tab', width=15)
+
+        # Notebook para trocar entre abas
+        self.notebook = Notebook(style='custom.TNotebook')
+        self.notebook.pack(expand=True, fill='both')
+        
+        # lista com a referencia das imagens, é necessario pra imagem se manter no loop principal, se não ela é criada e jogada fora
+        self.image_references = []
+
+        # Itens do gacha
+        self.firefly_pulls = ["imagens/firefly_pull.png"]
+        self.cone_pulls = ["imagens/firefly_cone.png"]
+        self.standard_pulls = ["imagens/welt_pull.png"]
+
+        self.fila = [] #lista do que você tirou no gacha
+        self.current_10x_index = 0  # index que irá aumentar de 1 em 1 para pegar os itens da lista um por vez
+
+        self.create_tabs()
+
+    def create_tabs(self): #cria as abas do notebook
+        self.create_tab("Firefly", "imagens/firefly_banner.png", lambda: self.pull1x(self.firefly_pulls), lambda: self.pull10x(self.firefly_pulls)) # firefly banner
+        self.create_tab("Light Cone", "imagens/cone_banner.png", lambda: self.pull1x(self.cone_pulls), lambda: self.pull10x(self.cone_pulls)) # light cone banner
+        self.create_standard_tab("Standard", "imagens/Stellar_Warp.png", lambda: self.pull1x(self.standard_pulls), lambda: self.pull10x(self.standard_pulls)) # standard banner
+
+    def create_tab(self, title, banner_image_path, pull1x_command, pull10x_command): # tab da firefly e light cone por enquanto
+        tab = Frame(self.notebook, width=200, height=200)
+        img = ImageTk.PhotoImage(Image.open("imagens/firefly_icon.jpg"))
+        self.image_references.append(img)
+        self.notebook.add(tab, text=title, padding=10, image=img, compound="center")
+        banner_image = ImageTk.PhotoImage(Image.open(banner_image_path))
+        self.image_references.append(banner_image)
+        tk.Label(tab, image=banner_image).pack()
+        self.create_buttons(tab, pull1x_command, pull10x_command)
+
+    def create_standard_tab(self, title, banner_image_path, pull1x_command, pull10x_command): # tab standard
+        tab = Frame(self.notebook, width=200, height=200)
+        self.notebook.add(tab, text=title, padding=10)
+        warp = Image.open(banner_image_path)
+        warp_resized = warp.resize((700, 387)) # resize da imagem do standard, ela era muito grande
+        warp_image = ImageTk.PhotoImage(warp_resized)
+        self.image_references.append(warp_image)
+        tk.Label(tab, image=warp_image).pack()
+        self.create_buttons(tab, pull1x_command, pull10x_command)
+
+    def create_buttons(self, tab, pull1x_command, pull10x_command): #cria a base dos botões, recebe argumentos pra mudar o comando dos botões 
+        frame = Frame(tab, bootstyle="dark")
+        frame.pack(side="bottom", fill="both")
+        button1 = Button(tab, bootstyle="dark", text="Warp 1x", command=pull1x_command)
+        button10 = Button(tab, bootstyle="dark", text="Warp 10x", command=pull10x_command)
+        button1.pack(in_=frame, anchor='s', side='left', fill="both", expand=True)
+        button10.pack(in_=frame, anchor='s', side='right', fill="both", expand=True)
+
+    def pull1x(self,x): # 1 tentativa de gacha
+        print("1 pull")
+        resultado = random.choice(x)
+        print(resultado)
+        self.show_gif_and_result(resultado)
+
+    def pull10x(self,x): # 10 tentativas de gacha
+        print("10 pulls")
+        self.fila = [random.choice(x) for _ in range(10)]
+        print(self.fila)
+        self.current_10x_index = 0  # reseta o index caso não seja a primeira vez que você está usando o comando
+        self.show_gif_and_result(self.fila)
+
+    def show_gif_and_result(self, resultado): # mostra o gif e em seguida chama a função de mostrar o resultado
+        new_window = tk.Toplevel(self.root)
+        new_window.resizable(False, False) # evita que o usuario mude o tamanho da janela
+        gif_path = "imagens/qiqi.gif" if "imagens/qiqi_pull.png" in resultado else "imagens/5_estrelas.gif" if "imagens/firefly_pull.png" in resultado else "imagens/3_estrelas.gif"
+  
+        # chama uma classe que comprime e junta os frames do gif de forma com que ele apareça direito, sem pontos vazios
+        gif = AnimatedGif(new_window, gif_path, loop=False, on_complete=lambda: self.show_result(new_window, resultado)) # não funciona sem lambda por algum motivo
+        gif.pack()
+
+    def show_result(self, window, result): # define qual imagem será mostrada na tela de resultado e envia para a função que cria a tela de resultado
+        if isinstance(result, str):  # 1 pull, verifica se a variavel resultado é uma string
+            result_image_path = result
+            self.create_result_window(window, result_image_path)
+        else:  # 10 pull, caso ela for uma lista e não uma string
+            if self.current_10x_index < len(result): # enquanto aquele index lá de trás for menor que o tamanho da lista
+                result_image_path = result[self.current_10x_index]
+                self.create_result_window(window, result_image_path)
+                self.current_10x_index += 1 # aumenta o index lá de trás
+            else:
+                self.close_all_windows() # caso o index seja igual ou maior que o tamanho da lista, fecha as abas
+
+    def create_result_window(self, window, image_path): # cria a tela de resultado com a informação da função anterior
+        result_window = tk.Toplevel(window) 
+        result_image = ImageTk.PhotoImage(Image.open(image_path)) # carrega a imagem do resultado
+        
+        # tentando limitar a imagem a um frame pra ela parar de mudar de tamanho, não funcionou 
+        img_frame = Frame(result_window, width=600, height=300)
+        img_frame.pack(fill="both",expand=True)
+        img_frame.propagate(False) # evita que a frame aumente de tamanho baseado nas imagens
+        
+    
+        label = tk.Label(img_frame, image=result_image) # joga a imagem na tela
+        label.pack()
+        self.image_references.append(result_image) # jogando a imagem pras referencias pra ela n sumir
+        
+        frame = Frame(result_window, bootstyle="dark")
+        frame.pack(side="bottom", fill="both")
+        
+        next_button = Button(result_window, bootstyle="light", text="Next", command=lambda: self.proximo(result_window, label)) # botão que chama a função proximo
+        next_button.pack(in_=frame, anchor='s', side='left', fill="both", expand=True)
+
+    def proximo(self, result_window, label): # troca para a proxima imagem
+        if self.current_10x_index < len(self.fila): # checa se o index lá de trás é menor que o tamanho da lista
+            result_image_path = self.fila[self.current_10x_index] # vai se referir ao item da lista baseado no index
+            new_image = ImageTk.PhotoImage(Image.open(result_image_path))
+            self.image_references.append(new_image)
+            label.config(image=new_image) # reconfigura a imagem da label para a nova
+            self.current_10x_index += 1
+        else:
+            self.close_all_windows()
+
+    def close_all_windows(self):
+        for window in self.root.winfo_children(): # winfo_children() se refere a todas as janelas criadas a partir de outra
+            if isinstance(window, tk.Toplevel): # verifica se a variavel window é uma nova janela tk
+                window.destroy() # mata as criança :) 
+
+def main():
+    root = tk.Tk()
+    app = WarpSimulator(root)
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
