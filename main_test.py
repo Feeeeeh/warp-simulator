91b75c63331eb4ac51e3020d3db93b63f22ca9c2
HoundVales <lukkas7425@gmail.com>
2024-07-24 14:57:30 -0300

    new db v3

--- main_test.py
+++ main_test.py
@@ -3,15 +3,15 @@ from ttkbootstrap import Style, Frame, Button, Notebook
 from PIL import ImageTk, Image
 from gif_loader import AnimatedGif
 import random
-import mysql.connector
-import db_test
 
 class WarpSimulator:
-    def __init__(self, root):
+    def __init__(self, root): 
         # cria a janela, seta um titulo e o tamanho dela
         self.root = root
-        self.root.title("Warp Simulator")
-        self.root.geometry("800x500")
+        self.root.title("Warp Simulator") 
+        self.root.geometry("800x500") 
+        root.resizable(False, False) # evita que o usuario mude o tamanho da janela 
+
 
         # Estiliação da janela no estilo darkly na biblioteca ttkbootstrap
         self.style = Style(theme='darkly')
@@ -21,7 +21,9 @@ class WarpSimulator:
         # Notebook para trocar entre abas
         self.notebook = Notebook(style='custom.TNotebook')
         self.notebook.pack(expand=True, fill='both')
-        self.image_references = [] # lista com a referencia das imagens, é necessario pra imagem se manter no loop principal, se não ela é criada e jogada fora
+        
+        # lista com a referencia das imagens, é necessario pra imagem se manter no loop principal, se não ela é criada e jogada fora
+        self.image_references = []
 
         # Itens do gacha
         self.firefly_pulls = ["imagens/firefly_pull.png", "imagens/bailu_pull.png", "imagens/himeko_pull.png","imagens/welt_pull.png","imagens/yanqing_pull.png","imagens/clara_pull.png","imagens/bronya_pull.png",
@@ -29,21 +31,6 @@ class WarpSimulator:
 
         self.fila = [] #lista do que você tirou no gacha
         self.current_10x_index = 0  # index que irá aumentar de 1 em 1 para pegar os itens da lista um por vez
-
-        # Conectar ao banco de dados MySQL
-        self.conexao = mysql.connector.connect(
-            host="localhost",
-            user="root",
-            password="",
-            database="hsr"
-        )
-
-        # Verificar se a conexão foi bem sucedida
-        if self.conexao.is_connected():
-            print('Conexão ao banco de dados MySQL estabelecida.')
-            self.cursor = self.conexao.cursor()
-        else:
-            print('Erro ao conectar ao banco de dados MySQL.')
 
         self.create_tabs()
 
@@ -54,7 +41,9 @@ class WarpSimulator:
 
     def create_tab(self, title, banner_image_path, pull1x_command, pull10x_command): # tab da firefly e light cone por enquanto
         tab = Frame(self.notebook, width=200, height=200)
-        self.notebook.add(tab, text=title, padding=10)
+        img = ImageTk.PhotoImage(Image.open("imagens/firefly_icon.jpg"))
+        self.image_references.append(img)
+        self.notebook.add(tab, text=title, padding=10, image=img, compound="center")
         banner_image = ImageTk.PhotoImage(Image.open(banner_image_path))
         self.image_references.append(banner_image)
         tk.Label(tab, image=banner_image).pack()
@@ -83,7 +72,6 @@ class WarpSimulator:
         resultado = random.choice(self.firefly_pulls)
         print(resultado)
         self.show_gif_and_result(resultado)
-        db_test.escolher_operacao(1, self.cursor)  # Chama a operação de banco de dados correspondente
 
     def pull10x(self): # 10 tentativas de gacha
         print("10 pulls")
@@ -91,11 +79,11 @@ class WarpSimulator:
         print(self.fila)
         self.current_10x_index = 0  # reseta o index caso não seja a primeira vez que você está usando o comando
         self.show_gif_and_result(self.fila)
-        db_test.escolher_operacao(2, self.cursor)  # Chama a operação de banco de dados correspondente
 
     def show_gif_and_result(self, resultado): # mostra o gif e em seguida chama a função de mostrar o resultado
         new_window = tk.Toplevel(self.root)
-        gif_path = "qiqi" if "imagens/qiqi_pull.png" in resultado else "imagens/5_estrelas.gif" if "firefly1" or "firefly2" or "firefly3" or "firefly4" in resultado else "imagens/3_estrelas.gif"
+        new_window.resizable(False, False) # evita que o usuario mude o tamanho da janela
+        gif_path = "imagens/qiqi.gif" if "imagens/qiqi_pull.png" in resultado else "imagens/5_estrelas.gif" if "imagens/firefly_pull.png" in resultado else "imagens/3_estrelas.gif"
   
         # chama uma classe que comprime e junta os frames do gif de forma com que ele apareça direito, sem pontos vazios
         gif = AnimatedGif(new_window, gif_path, loop=False, on_complete=lambda: self.show_result(new_window, resultado)) # não funciona sem lambda por algum motivo
@@ -114,13 +102,15 @@ class WarpSimulator:
                 self.close_all_windows() # caso o index seja igual ou maior que o tamanho da lista, fecha as abas
 
     def create_result_window(self, window, image_path): # cria a tela de resultado com a informação da função anterior
-        result_window = tk.Toplevel(window)
+        result_window = tk.Toplevel(window) 
         result_image = ImageTk.PhotoImage(Image.open(image_path)) # carrega a imagem do resultado
         
         # tentando limitar a imagem a um frame pra ela parar de mudar de tamanho, não funcionou 
-        img_frame = Frame(result_window)
+        img_frame = Frame(result_window, width=600, height=300)
         img_frame.pack(fill="both",expand=True)
+        img_frame.propagate(False) # evita que a frame aumente de tamanho baseado nas imagens
         
+    
         label = tk.Label(img_frame, image=result_image) # joga a imagem na tela
         label.pack()
         self.image_references.append(result_image) # jogando a imagem pras referencias pra ela n sumir
@@ -146,17 +136,10 @@ class WarpSimulator:
             if isinstance(window, tk.Toplevel): # verifica se a variavel window é uma nova janela tk
                 window.destroy() # mata as criança :) 
 
-    def close(self):
-        # Fecha o cursor e a conexão
-        if self.conexao.is_connected():
-            self.cursor.close()
-            self.conexao.close()
-            print('Conexão ao banco de dados MySQL encerrada.')
-        self.root.destroy()
-
-if __name__ == "__main__":
+def main():
     root = tk.Tk()
     app = WarpSimulator(root)
-    root.protocol("WM_DELETE_WINDOW", app.close)
     root.mainloop()
-    
+
+if __name__ == "__main__":
+    main()  
