a4483f112e7d7a0c9de7094b115758a43507e24d
Feeeeeh <klitzketech@gmail.com>
2024-08-12 15:14:09 -0300

    tudo foi comentado bonitinho

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from mysql.connector import errorcode
 from new_db import DatabaseManager
 
 
-# Database configuration
+# Config base pra acessar o database
 config = {
     'user': 'root',
     'password': '',
@@ -12,6 +12,7 @@ config = {
 
 database_name = 'hsr'
 
+# pega a database chamada "hsr"
 def database_exists(cursor, db_name):
     cursor.execute("SHOW DATABASES LIKE '%s'" % db_name)
     return cursor.fetchone()
@@ -19,19 +20,19 @@ def database_exists(cursor, db_name):
 try:
     conn = mysql.connector.connect(**config)
     cursor = conn.cursor()
-    if not database_exists(cursor, database_name):
+    if not database_exists(cursor, database_name): # se a database não existir, cria ela
         db_manager = DatabaseManager()
         db_manager.create_tables()
         db_manager.insert_data()
         print("Database criado")
     else:
-        print("Acessando database")
+        print("Acessando database") # se existir apenas segue o codigo
 except mysql.connector.Error as err:
     if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-        print("Something is wrong with your user name or password")
+        print("Acesso negado.")
     elif err.errno == errorcode.ER_BAD_DB_ERROR:
-        print("Database does not exist")
+        print("Database não existe.")
     else:
         print(err)
     
-from login_gacha import *
+from login_gacha import * # roda o arquivo login_gacha
